a4c5b9aaab3495a028f5addb007ec30d7c46a851
Brandon Chao <bchao40@stuy.edu>
2023-02-17 15:35:49 -0500

    attempted, palindrome check (particularly getting number length) is not working

--- Problem4.java
+++ Problem4.java
@@ -1,5 +1,25 @@
 public class Problem4{
   public static void main(String[] args){
-    
+    int greatestValue = 10000;
+    for (int i = 100; i < 1000; i++){
+      for (int j = 100; j < 1000; j++){
+        int checking = j * i;
+        System.out.println(checking);
+        boolean isPalindrome = true;
+        if (checking > greatestValue){
+          int checkingLength = (int) (Math.log10(checking) + 1);
+          System.out.println(checkingLength);
+          for (int k = 0; k < checkingLength; k++){
+            if (!(i % Math.pow(10, checkingLength - k) == j % Math.pow(10, k))){
+              isPalindrome = false;
+            }
+          }
+          if (isPalindrome){
+            greatestValue = checking;
+          }
+        }
+      }
+    }
+    System.out.println(greatestValue);
   }
 }
