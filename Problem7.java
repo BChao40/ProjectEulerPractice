bdc791d5d818c2c9da2fc107858640647062a350
Brandon Chao <bchao40@stuy.edu>
2023-02-19 11:42:09 -0500

    finished problem 7

--- Problem7.java
+++ Problem7.java
@@ -1,24 +1,28 @@
-import java.lang.Math;
 public class Problem7{
   /*
   Some things to note:
   -The value of the nth prime number is always at least 2x greater than n, exceptions being the first 4 prime numbers. Because we are looking at a
   large n value, we can expect the value of the nth prime to be at least 2n.
   -There is no need to check even numbers, thus we can increment by 2.
-  -To check if a number is prime, we only have to check factors up until its square root.
+  -To check if a number is prime, we only have to check factors up until half its value.
   -Because we need to find the nth prime number, it's reasonable to assume that we have to know every prime number before it.
   */
-
-  int n = 0;
-  int i = 1;
-  while (n < 10001){
-    i++;
-    boolean isPrime = true;
-    for (int j = 0; j < Math.sqrt(i); j++){
-      if (i % j == 0){
-        isPrime = false;
+  public static void main(String[] args){
+    int n = 2;
+    int i = 3;
+    while (n < 10001){
+      i += 2;
+      boolean isPrime = true;
+      for (int j = 2; j < i / 2; j++){
+        if (i % j == 0){
+          isPrime = false;
+        }
+      }
+      if(isPrime){
+        n++;
+        System.out.println(i);
       }
     }
-
+    System.out.println(i);
   }
 }
