8a326eeffa9a08c4edc39b8e1293e2bd3037c206
Brandon Chao <bchao40@stuy.edu>
2023-03-28 18:21:44 -0400

    Printed loops that iterate through each digit of each line in order

--- Problem13.java
+++ Problem13.java
@@ -2,8 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Problem13{
-  public static void main(String[] args){
+  public static void main(String[] args) throws FileNotFoundException{
     File myFile = new File("Input13.txt");
-    Scanner myScanner = new Scanner(myFile);
+
+    int[] tenDigits = new int[10];
+
+    for (int i = 1; i <= 10; i++){
+      Scanner myScanner = new Scanner(myFile);
+      String printLine = "";
+      while (myScanner.hasNextLine()){
+        String currentLine = myScanner.nextLine();
+        int currentDigit = Integer.parseInt(currentLine.substring(currentLine.length() - i, currentLine.length() - i + 1));
+        printLine = printLine + currentDigit;
+      }
+      System.out.println(printLine);
+    }
   }
 }
