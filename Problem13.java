2a74e584de43d2a8ccf58d1919a6e5b081b622dc
Brandon Chao <bchao40@stuy.edu>
2023-03-28 19:49:35 -0400

    Theoretically works but the issue is that the digits before the first 10 are ignored, thus being inaccurate; it would be accurate if the digits before the first ten were all 0, but that's not exactly how numbers work

--- Problem13.java
+++ Problem13.java
@@ -11,17 +11,43 @@ public class Problem13{
       Scanner myScanner = new Scanner(myFile);
       while (myScanner.hasNextLine()){
         String currentLine = myScanner.nextLine();
-        int currentDigit = Integer.parseInt(currentLine.substring(currentLine.length() - i, currentLine.length() - i + 1));
+        int currentDigit = Integer.parseInt(currentLine.substring(i - 1, i));
         tenDigits[i - 1] += currentDigit;
+
+
+        /*
         if (tenDigits[i - 1] > 9){
-          if (i - 1 != 9){
-            tenDigits[i] = tenDigits[i - 1] / 10;
+          if (i - 1 != 0){
+            tenDigits[i] += tenDigits[i - 1] / 10;
           }
           tenDigits[i - 1] %= 10;
         }
+        */
       }
     }
 
+    for (int i = 0; i < tenDigits.length; i++){
+      //carryOver(tenDigits, i);
+    }
     System.out.println(Arrays.toString(tenDigits));
+  }
+
+  public static void carryOver(int[] arr, int index){
+    int BiggestDigitPlace = 10;
+    int value = arr[index];
+    while (arr[index] > BiggestDigitPlace){
+      BiggestDigitPlace *= 10;
+    }
+
+    for (int i = 0; i < Integer.toString(BiggestDigitPlace).length() - 1; i++){
+      if (index + i < arr.length){ // checks if carrying over is valid
+        if (i == 0){ // checks if it's complete replacement or not
+          arr[index + i] = (value / (int) Math.pow(10, i)) % 10; // carries over/replaces any digits needed to be carried over/replaced
+        }
+        else{
+          arr[index + i] += (value / (int) Math.pow(10, i)) % 10;
+        }
+      }
+    }
   }
 }
