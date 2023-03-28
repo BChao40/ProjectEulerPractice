0a7000bfdbd3bc7c912697c27cfa2605be78adce
Brandon Chao <bchao40@stuy.edu>
2023-03-28 15:38:46 -0400

    Worked on problem 13

---
+++ Problem13.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Problem13{
+  public static void main(String[] args){
+    File myFile = new File("Input13.txt");
+    Scanner myScanner = new Scanner(myFile);
+  }
+}
