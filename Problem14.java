1306ff47447a528038eb5cdaa3a4d2bdbd5c0154
Brandon Chao <bchao40@stuy.edu>
2023-03-29 15:38:44 -0400

    Created naive method, doesnt work?

--- Problem14.java
+++ Problem14.java
@@ -1,16 +1,17 @@
 public class Problem14{
   public static void main(String[] args){
-    System.out.println(recursion(1));
+    //System.out.println(recursion(1));
+    System.out.println(naive());
   }
 
-  public static int recursion(int x){
+  public static int recursion(int x, int counter){
     if (x * 2 >= 1000000 || (x - 1) / 3 >= 1000000){
       return x;
     }
     else{
       if (x % 2 == 0){
-        int typeA = recursion(x * 2);
-        int typeB = recursion((x - 1) / 3);
+        int typeA = recursion(x * 2, counter++);
+        int typeB = recursion((x - 1) / 3, counter++);
 
         if (typeA > typeB){
           return typeB;
@@ -20,9 +21,37 @@ public class Problem14{
         }
       }
       else{
-        return recursion(x * 2);
+        return recursion(x * 2, counter++);
       }
     }
 
+  }
+
+  public static int collatz(int x){
+    if (x % 2 == 0){
+      return x / 2;
+    }
+    else{
+      return x * 3 + 1;
+    }
+  }
+
+  public static int naive(){
+    int startValue = 0;
+    int counter = 0;
+    for (int i = 1; i <= 1000000; i++){
+      int current = i;
+      int tempCount = 0;
+      while (collatz(current) > 1){
+        current = collatz(current);
+        tempCount++;
+      }
+      if (tempCount > counter){
+        startValue = i;
+        counter = tempCount;
+      }
+    }
+
+    return startValue;
   }
 }
