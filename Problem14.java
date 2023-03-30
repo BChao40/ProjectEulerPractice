7a50a1bb20cc096efef250cc52f7e59a06574eef
Brandon Chao <bchao40@stuy.edu>
2023-03-29 20:23:38 -0400

    Removed recursive method, impossible to figure solution through backtracking if numbers after the start can be over 1 mil (no solid base case)

--- Problem14.java
+++ Problem14.java
@@ -5,29 +5,6 @@ public class Problem14{
 
   }
 
-  public static long recursion(long x, int counter){
-    if (x * 2 >= 1000000 || (x - 1) / 3 >= 1000000){
-      return x;
-    }
-    else{
-      if (x % 2 == 0){
-        long typeA = recursion(x * 2, counter++);
-        long typeB = recursion((x - 1) / 3, counter++);
-
-        if (typeA > typeB){
-          return typeB;
-        }
-        else{
-          return typeA;
-        }
-      }
-      else{
-        return recursion(x * 2, counter++);
-      }
-    }
-
-  }
-
   public static long collatz(long x){
     if (x % 2 == 0){
       return x / 2;
