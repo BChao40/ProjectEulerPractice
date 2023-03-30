1fbc1538e36f8454c61c4a02a12656456db459dc
Brandon Chao <bchao40@stuy.edu>
2023-03-29 20:19:12 -0400

    Using longs fixes the limited number range issue with the naive approach and works, will work on a more complex approach

--- Problem14.java
+++ Problem14.java
@@ -1,32 +1,18 @@
 public class Problem14{
   public static void main(String[] args){
     //System.out.println(recursion(1));
-    //System.out.println(naive());
-
-      int n = 40;
-      int value = 40;
-      int tempCount = 0;
-      System.out.println(value);
-      while (collatz(value) > 1){
-        System.out.println(collatz(value));
-        value = collatz(value);
-        tempCount++;
-      }
-
-      if (tempCount > 1){
-        System.out.println("Value: " + n + " and TempCount: " + tempCount);
-      }
+    System.out.println(naive());
 
   }
 
-  public static int recursion(int x, int counter){
+  public static long recursion(long x, int counter){
     if (x * 2 >= 1000000 || (x - 1) / 3 >= 1000000){
       return x;
     }
     else{
       if (x % 2 == 0){
-        int typeA = recursion(x * 2, counter++);
-        int typeB = recursion((x - 1) / 3, counter++);
+        long typeA = recursion(x * 2, counter++);
+        long typeB = recursion((x - 1) / 3, counter++);
 
         if (typeA > typeB){
           return typeB;
@@ -42,7 +28,7 @@ public class Problem14{
 
   }
 
-  public static int collatz(int x){
+  public static long collatz(long x){
     if (x % 2 == 0){
       return x / 2;
     }
@@ -51,12 +37,12 @@ public class Problem14{
     }
   }
 
-  public static int naive(){
-    int startValue = 0;
-    int counter = 0;
-    for (int i = 1; i <= 1000000; i++){
-      int current = i;
-      int tempCount = 0;
+  public static long naive(){
+    long startValue = 0;
+    long counter = 0;
+    for (long i = 1; i <= 1000000; i++){
+      long current = i;
+      long tempCount = 0;
       while (collatz(current) > 1){
         current = collatz(current);
         tempCount++;
