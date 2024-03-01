03d5feb512aef35662d0c64bd5c5f8499557dbb5
Brandon Chao <bchao40@stuy.edu>
2024-03-01 11:29:30 -0500

    Problem 15 complete

--- Problem15.java
+++ Problem15.java
@@ -28,14 +28,14 @@ public class Problem15{
     //you can use combinatorics instead because you know for any nxn grid, there is a total of 2n moves to be made to reach the bottom right corner from the top left corner,
     //and there is n moves going right and n moves going left. you can use the combination formula C(n,k) = n! / (k! * (n-k)!)
 
-    public static long combination(int n, int k){ // for a 20x20 grid, n should be 40 because of 40 moves to be made and k should be 20 (finding combinations of the 20 of either down or right will inherently choose the 20 moves of the other type)
-        return factorial(n) / (factorial(k) * factorial(n-k));
+    public static BigInteger combination(int n, int k){ // for a 20x20 grid, n should be 40 because of 40 moves to be made and k should be 20 (finding combinations of the 20 of either down or right will inherently choose the 20 moves of the other type)
+        return factorial(n).divide(factorial(k).multiply(factorial(n-k)));
     }
 
-    public static long factorial(int x){
-        long result = 1;
-        for (long i = x; i >= 2; i--){
-            result *= i;
+    public static BigInteger factorial(int x){
+        BigInteger result = BigInteger.ONE;
+        for (int i = x; i >= 2; i--){
+            result = result.multiply(BigInteger.valueOf(i));
         }
         return result;
     }
